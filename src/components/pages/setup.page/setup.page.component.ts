8dfbecc5a08c41aebdb120b604370cf1375d47d5
renztp <tullaorenz@gmail.com>
2024-10-16 10:15:30 +0800

    added updates to setup page

--- src/components/pages/setup.page/setup.page.component.ts
+++ src/components/pages/setup.page/setup.page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-setup.page',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ButtonModule],
   templateUrl: './setup.page.component.html',
   styleUrl: './setup.page.component.scss'
 })
