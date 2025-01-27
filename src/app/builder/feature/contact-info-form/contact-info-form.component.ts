44918f082e3155748233eabcc7290213ecb6e936
renztp <tullaorenz@gmail.com>
2025-01-27 16:05:50 +0800

    added more changes for the contact-info form

--- src/app/builder/feature/contact-info-form/contact-info-form.component.ts
+++ src/app/builder/feature/contact-info-form/contact-info-form.component.ts
@@ -1,12 +1,17 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { SocialsSelectorComponent } from "../../ui/socials-selector/socials-selector.component";
-import { FormGroup } from "@angular/forms";
+import { FormGroup, ReactiveFormsModule, UntypedFormBuilder } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+
+enum ContactInfoPhases {
+  SocialsSelectionPhase = 0,
+  ContactInfoFormPhase = 1,
+}
 
 @Component({
   selector: 'app-contact-info-form',
   standalone: true,
-  imports: [SocialsSelectorComponent, CommonModule],
+  imports: [SocialsSelectorComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './contact-info-form.component.html',
   styleUrl: './contact-info-form.component.scss'
 })
@@ -14,10 +19,36 @@ export class ContactInfoFormComponent {
   @Output() next = new EventEmitter<void>();
   @Output() changed = new EventEmitter<FormGroup>();
   selectedSocials: string[] = [];
+  formGroup: FormGroup;
+  phase: ContactInfoPhases;
 
-  constructor() {}
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+  ) {
+    this.phase = ContactInfoPhases.SocialsSelectionPhase;
+    this.formGroup = this.formBuilder.group({
+      contacts: this.formBuilder.array([]),
+    })
+  }
+
+  get contacts() {
+    return this.formGroup.get('contacts') as FormGroup;
+  }
+
+  finishSocialsSelectionPhase() {
+    this.phase = ContactInfoPhases.ContactInfoFormPhase;
+  }
+
+  revertToSocialsSelectionPhase() {
+    this.phase = ContactInfoPhases.SocialsSelectionPhase;
+  }
 
   onSocialsToggled(socials: string[]) {
     this.selectedSocials = socials;
+
+    // const newContact = this.formBuilder.group({
+
+
+    // this.contacts.push()
   }
 }
