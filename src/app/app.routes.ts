7d1d26403b8c7484f7c998ada0ed8fb1459b79f9
renztp <tullaorenz@gmail.com>
2025-02-16 02:14:14 +0800

    added a small message at bottom of setup page component, fixed font bug error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { BuilderPageComponent } from './builder/feature/builder-page/builder.page';
 import { LayoutChooserPageComponent } from './layout-chooser/feature/layout-chooser.page';
-import { PreviewPageComponent } from './builder/feature/preview-page/preview.page';
 import { SetupPageComponent } from './setup/setup.page.component';
 
 export const routes: Routes = [
@@ -17,10 +16,6 @@ export const routes: Routes = [
     //     component: BasicInfoFormComponent
     //   }
     // ]
-  },
-  {
-    path: 'preview',
-    component: PreviewPageComponent
   },
   { path: '**', redirectTo: '' }
 ];
