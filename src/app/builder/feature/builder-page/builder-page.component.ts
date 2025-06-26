76991fb102436d889e655c69246d43c37962d43f
renztp <tullaorenz@gmail.com>
2025-06-26 20:20:50 +0800

    added initial functions for navigating routes on sidebar

--- src/app/builder/feature/builder-page/builder-page.component.ts
+++ src/app/builder/feature/builder-page/builder-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { BuilderNavbarComponent } from '../../ui/builder-navbar/builder-navbar.component';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { BuilderActionBarComponent } from '../../ui/builder-action-bar/builder-action-bar.component';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-builder-page',
@@ -11,5 +12,35 @@ import { BuilderActionBarComponent } from '../../ui/builder-action-bar/builder-a
   standalone: true,
 })
 export class BuilderPageComponent {
-  constructor() {}
+  steps: string[] = [];
+  currentIndex = 0;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    const childRoutes = this.route.routeConfig?.children || [];
+    console.log({ childRoutes });
+
+    this.router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe(() => {
+      const currentChildPath = this.route.firstChild?.snapshot.routeConfig?.path || '';
+      this.currentIndex = this.steps.indexOf(currentChildPath);
+    });
+  }
+
+  goToNext() {
+    if (this.currentIndex < this.steps.length - 1) {
+      const nextPath = this.steps[this.currentIndex + 1];
+      this.router.navigate([nextPath], { relativeTo: this.route });
+    }
+  }
+
+  goToPrevious() {
+    if (this.currentIndex > 0) {
+      const prevPath = this.steps[this.currentIndex - 1];
+      this.router.navigate([prevPath], { relativeTo: this.route });
+    }
+  }
 }
