5c9c5286e7871071f3a61cae933218cf45a7a281
renztp <tullaorenz@gmail.com>
2025-07-11 18:59:59 +0800

    added saving-indicator component

---
+++ src/app/shared/ui/saving-indicator/saving-indicator.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saving-indicator',
+  imports: [],
+  template: ` <p>saving-indicator works!</p> `,
+  styles: ``,
+})
+export class SavingIndicatorComponent {}
